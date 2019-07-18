f11a38841ac146eef6381320f0db88774db252c6
vuhuykhoi <vhk.forwork@gmail.com>
2019-07-18 16:39:21 +0700

    like/unlike
     Author:    vuhuykhoi <vhk.forwork@gmail.com>
     Date:      Sun Jul 28 12:00:00 2019 +0700

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
-//= require_tree ../../../vendor/assets/javascripts/customer/js
+//= require customer/js/home
+//= require customer/js/typed.js
+//= require customer/js/bootstrap.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
