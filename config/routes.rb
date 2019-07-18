58ff572e637a2833b9a8ee4efe8c7ba79056a2c9
vuhuykhoi <vhk.forwork@gmail.com>
2019-07-18 16:39:21 +0700

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
+  root "static_pages#home"
   scope :admin do
     root "admin#index"
   end
+
+  get "signup", to: "users#new"
+  post "signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
 end
